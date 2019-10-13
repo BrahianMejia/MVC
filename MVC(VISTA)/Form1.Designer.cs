f848a5ac3589a994ac3f85826b3dc8cf1ab81fa3
Brahian Mejia <brayantqm123@gmail.com>
2019-10-13 11:36:26 -0500

    Actualización del proyecto MVC.

---
+++ MVC(VISTA)/Form1.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace MVC_VISTA_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.dtregistros = new System.Windows.Forms.DataGridView();
+            this.Id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Nombre = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Telefono = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtcodigo = new System.Windows.Forms.TextBox();
+            this.txtnombre = new System.Windows.Forms.TextBox();
+            this.lblnombre = new System.Windows.Forms.Label();
+            this.txttelefono = new System.Windows.Forms.TextBox();
+            this.lbltelefono = new System.Windows.Forms.Label();
+            this.btnguardar = new System.Windows.Forms.Button();
+            this.btnguardarcambios = new System.Windows.Forms.Button();
+            this.btneliminar = new System.Windows.Forms.Button();
+            this.btncancelar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dtregistros)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(177, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(234, 33);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Datos de Contáctos";
+            // 
+            // dtregistros
+            // 
+            this.dtregistros.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtregistros.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Id,
+            this.Nombre,
+            this.Telefono});
+            this.dtregistros.Location = new System.Drawing.Point(12, 224);
+            this.dtregistros.Name = "dtregistros";
+            this.dtregistros.Size = new System.Drawing.Size(569, 234);
+            this.dtregistros.TabIndex = 1;
+            this.dtregistros.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dtregistros_CellClick);
+            // 
+            // Id
+            // 
+            this.Id.DataPropertyName = "Id";
+            this.Id.HeaderText = "Código";
+            this.Id.Name = "Id";
+            // 
+            // Nombre
+            // 
+            this.Nombre.DataPropertyName = "Nombre";
+            this.Nombre.HeaderText = "Nombre";
+            this.Nombre.Name = "Nombre";
+            this.Nombre.Width = 250;
+            // 
+            // Telefono
+            // 
+            this.Telefono.DataPropertyName = "Telefono";
+            this.Telefono.HeaderText = "Teléfono";
+            this.Telefono.Name = "Telefono";
+            this.Telefono.Width = 175;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Código:";
+            // 
+            // txtcodigo
+            // 
+            this.txtcodigo.Location = new System.Drawing.Point(85, 79);
+            this.txtcodigo.Name = "txtcodigo";
+            this.txtcodigo.Size = new System.Drawing.Size(100, 20);
+            this.txtcodigo.TabIndex = 3;
+            // 
+            // txtnombre
+            // 
+            this.txtnombre.Location = new System.Drawing.Point(85, 105);
+            this.txtnombre.Name = "txtnombre";
+            this.txtnombre.Size = new System.Drawing.Size(159, 20);
+            this.txtnombre.TabIndex = 5;
+            // 
+            // lblnombre
+            // 
+            this.lblnombre.AutoSize = true;
+            this.lblnombre.Location = new System.Drawing.Point(12, 105);
+            this.lblnombre.Name = "lblnombre";
+            this.lblnombre.Size = new System.Drawing.Size(47, 13);
+            this.lblnombre.TabIndex = 4;
+            this.lblnombre.Text = "Nombre:";
+            // 
+            // txttelefono
+            // 
+            this.txttelefono.Location = new System.Drawing.Point(85, 131);
+            this.txttelefono.Name = "txttelefono";
+            this.txttelefono.Size = new System.Drawing.Size(100, 20);
+            this.txttelefono.TabIndex = 7;
+            // 
+            // lbltelefono
+            // 
+            this.lbltelefono.AutoSize = true;
+            this.lbltelefono.Location = new System.Drawing.Point(12, 131);
+            this.lbltelefono.Name = "lbltelefono";
+            this.lbltelefono.Size = new System.Drawing.Size(52, 13);
+            this.lbltelefono.TabIndex = 6;
+            this.lbltelefono.Text = "Teléfono:";
+            // 
+            // btnguardar
+            // 
+            this.btnguardar.Location = new System.Drawing.Point(15, 174);
+            this.btnguardar.Name = "btnguardar";
+            this.btnguardar.Size = new System.Drawing.Size(75, 23);
+            this.btnguardar.TabIndex = 8;
+            this.btnguardar.Text = "Guardar";
+            this.btnguardar.UseVisualStyleBackColor = true;
+            this.btnguardar.Click += new System.EventHandler(this.btnguardar_Click);
+            // 
+            // btnguardarcambios
+            // 
+            this.btnguardarcambios.Location = new System.Drawing.Point(96, 174);
+            this.btnguardarcambios.Name = "btnguardarcambios";
+            this.btnguardarcambios.Size = new System.Drawing.Size(134, 23);
+            this.btnguardarcambios.TabIndex = 9;
+            this.btnguardarcambios.Text = "Guardar Cambios";
+            this.btnguardarcambios.UseVisualStyleBackColor = true;
+            this.btnguardarcambios.Click += new System.EventHandler(this.btnguardarcambios_Click);
+            // 
+            // btneliminar
+            // 
+            this.btneliminar.Location = new System.Drawing.Point(248, 174);
+            this.btneliminar.Name = "btneliminar";
+            this.btneliminar.Size = new System.Drawing.Size(75, 23);
+            this.btneliminar.TabIndex = 10;
+            this.btneliminar.Text = "Eliminar";
+            this.btneliminar.UseVisualStyleBackColor = true;
+            this.btneliminar.Click += new System.EventHandler(this.btneliminar_Click);
+            // 
+            // btncancelar
+            // 
+            this.btncancelar.Location = new System.Drawing.Point(506, 174);
+            this.btncancelar.Name = "btncancelar";
+            this.btncancelar.Size = new System.Drawing.Size(75, 23);
+            this.btncancelar.TabIndex = 11;
+            this.btncancelar.Text = "Cancelar";
+            this.btncancelar.UseVisualStyleBackColor = true;
+            this.btncancelar.Click += new System.EventHandler(this.btncancelar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(589, 470);
+            this.Controls.Add(this.btncancelar);
+            this.Controls.Add(this.btneliminar);
+            this.Controls.Add(this.btnguardarcambios);
+            this.Controls.Add(this.btnguardar);
+            this.Controls.Add(this.txttelefono);
+            this.Controls.Add(this.lbltelefono);
+            this.Controls.Add(this.txtnombre);
+            this.Controls.Add(this.lblnombre);
+            this.Controls.Add(this.txtcodigo);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dtregistros);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dtregistros)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dtregistros;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Id;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Nombre;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Telefono;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtcodigo;
+        private System.Windows.Forms.TextBox txtnombre;
+        private System.Windows.Forms.Label lblnombre;
+        private System.Windows.Forms.TextBox txttelefono;
+        private System.Windows.Forms.Label lbltelefono;
+        private System.Windows.Forms.Button btnguardar;
+        private System.Windows.Forms.Button btnguardarcambios;
+        private System.Windows.Forms.Button btneliminar;
+        private System.Windows.Forms.Button btncancelar;
+    }
+}
+
---
+++ MVC(VISTA)/Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Controlador;
+
+
+namespace MVC_VISTA_
+{
+    public partial class Form1 : Form
+    {
+        ClsContacto objContacto = null;
+        ClsContactoMgr objContactoMgr = null;
+        DataTable Dtt;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void listar()
+        {
+            objContacto = new ClsContacto();
+            objContacto.opc = 1;
+            objContactoMgr = new ClsContactoMgr(objContacto);
+
+            Dtt = new DataTable();
+            Dtt = objContactoMgr.Listar();
+
+             if (Dtt.Rows.Count > 0)
+            {
+                dtregistros.DataSource = Dtt;
+            }
+        }
+
+        private void Guardar()
+        {
+            objContacto = new ClsContacto();
+            objContacto.opc = 2;
+            objContacto.Id = Convert.ToInt32(txtcodigo.Text);
+            objContacto.Nombre = txtnombre.Text;
+            objContacto.Telefono = txttelefono.Text;
+
+            objContactoMgr = new ClsContactoMgr(objContacto);
+            objContactoMgr.GuardarDatos();
+            MessageBox.Show("Contacto guardado exitosamente.", "¡Mensaje de comprobación!");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listar();
+            btnguardarcambios.Enabled = false;
+            btneliminar.Enabled = false;
+            txtcodigo.Focus();
+        }
+
+        private void btnguardar_Click(object sender, EventArgs e)
+        {
+            Guardar();
+            listar();
+            LimpiarCampos();
+        }
+
+        private void LimpiarCampos()
+        {
+            txtcodigo.Clear();
+            txtnombre.Clear();
+            txttelefono.Clear();
+            txtcodigo.Focus();
+        }
+
+        private void GuardarCambios()
+        {
+            objContacto = new ClsContacto();
+            objContacto.opc = 3;
+            objContacto.Id = Convert.ToInt32(txtcodigo.Text);
+            objContacto.Nombre = txtnombre.Text;
+            objContacto.Telefono = txttelefono.Text;
+
+            objContactoMgr = new ClsContactoMgr(objContacto);
+            objContactoMgr.GuardarDatos();
+            MessageBox.Show("Contacto actualizado exitosamente.", "¡Mensaje de comprobación!");
+        }
+
+        private void dtregistros_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            txtcodigo.Text = dtregistros.Rows[dtregistros.CurrentRow.Index].Cells[0].Value.ToString();
+            txtnombre.Text = dtregistros.Rows[dtregistros.CurrentRow.Index].Cells[1].Value.ToString();
+            txttelefono.Text = dtregistros.Rows[dtregistros.CurrentRow.Index].Cells[2].Value.ToString();
+
+            btnguardar.Enabled = false;
+            btnguardarcambios.Enabled = true;
+            txtcodigo.Enabled = false;
+            btneliminar.Enabled = true;
+        }
+
+        private void btnguardarcambios_Click(object sender, EventArgs e)
+        {
+            GuardarCambios();
+            listar();
+            btnguardar.Enabled = true;
+            btnguardarcambios.Enabled = false;
+            txtcodigo.Enabled = true;
+            LimpiarCampos();
+            btneliminar.Enabled = false;
+        }
+
+        public void Eliminar()
+        {
+            objContacto = new ClsContacto();
+            objContacto.opc = 4;
+            objContacto.Id = Convert.ToInt32(txtcodigo.Text);
+            objContactoMgr = new ClsContactoMgr(objContacto);
+
+            objContactoMgr.EliminarDatos();
+            MessageBox.Show("Contacto eliminado exitosamente.", "¡Mensaje de comprobación!");
+        }
+
+        private void btneliminar_Click(object sender, EventArgs e)
+        {
+            Eliminar();
+            listar();
+            btnguardar.Enabled = true;
+            btnguardarcambios.Enabled = false;
+            txtcodigo.Enabled = true;
+            btneliminar.Enabled = false;
+            LimpiarCampos();
+        }
+
+        private void btncancelar_Click(object sender, EventArgs e)
+        {
+            listar();
+            btnguardar.Enabled = true;
+            btnguardarcambios.Enabled = false;
+            btneliminar.Enabled = false;
+            txtcodigo.Enabled = true;
+            LimpiarCampos();
+        }
+    }
+}
